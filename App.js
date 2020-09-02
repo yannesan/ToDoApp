997ebd5fe0d7bc630a46c6afec736a34ab70d48b
yannesan <yanne.marzouk@epitech.eu>
2020-09-02 15:42:47 +0200

    Finish (ToDoApp classic with basic functions)

--- App.js
+++ App.js
@@ -63,9 +63,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   content: {
+    flex: 1,
     padding: 40,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   }
 });
+ 

45a36bc2a7b973188748ee2c3ccbe70689fad68c
yannesan <yanne.marzouk@epitech.eu>
2020-09-02 12:26:23 +0200

    Application ToDoApp - React Native

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Entete from './Components/Header';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on the switch', key :'3'}
+  ]); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Entete />
+        <View style={styles.content}>
+          { /* to form */ }
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <Text>{item.text}</Text>
+              )}
+            /> 
+          </View>
+        </View>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
